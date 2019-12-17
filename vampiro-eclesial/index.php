155804ee572767c71fdf7b18616131f668103436
vitor-joao-ti <vitor.joao.ti@gmail.com>
2019-12-17 11:09:24 -0300

    inputada index.php

---
+++ vampiro-eclesial/index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>O Vampiro Eclesial</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	</head>
+	<body>
+		<center><h1>Ad Infinitum</h1>
+		<i>O universo é o conjunto infinito <br> 
+		de medidas infinitamente imperfeitas</i></center>
+		<br>
+		<section class="blue1">
+			<h2>Signos</h2>
+				<p>Se não significamos nada <br>
+				Então tudo importa	<br>
+				Quando tu bateu em minha porta<br>
+				Sabia que a noite ía durar</p>
+
+				<p>Não há metaliguagem aqui <br>
+				Mas meta a lingua em minha boca<br>
+				A esperança ainda é pouca<br>
+				Ainda há muito o que fazer</p>
+
+				<p>As estrelas nos olham abismadas<br>
+				no escuro infinito do espaço-tempo<br>
+				Na tristeza de um momento<br>
+				Bem além do horizonte</p>
+
+				<p>Tu verás que essa viagem<br>
+				Valerá pela imagem<br>
+				Da nossa curta eternidade<br>
+				Então viva está verdade</p>
+
+				<p>Para encontrar teu sentido<br>
+				Nenhum tempo foi perdido<br>
+				Nenhum espaço excedido</p>
+
+				<p>O significado de significar<br>
+				É encontrar um novo motivo<br>
+				Para viver<br>
+				Cada vez que queremos morrer<br>
+				O significado de significar<br>
+				É aceitar que a vida não<br>
+				É só prazer</p>
+
+				<p>Então viva por viver<br>
+				Viva para encontrar<br>
+				Seu significado</p>
+		</section>
+		<section class="blue2">
+			<h2>Estrelas</h2>
+				<p>Não estou a ouvir estrelas <br>
+				Mesmo assim sou feliz em vê-las<br>
+				Refletidas no teu olhar</p>
+
+				<p>Enquanto sonham tranquilas <br>
+				Eu ainda hei de ouvi-las <br>
+				Nas lágrimas de um Quasar</p>
+
+				<p>Para então alcançá-las <br>
+				Num sutil cintilar <br>
+				E sob a luz de Pallas <br>
+				N'outra valsa estelar</p>
+
+				<p>Eu sei que teus olhos <br>
+				Irão me mostrar <br>
+				Cada estrela morta <br>
+				Que um dia hei de amar</p>
+		</section>
+		<section class="blue3">
+			<h2>Filosofia Quântica</h2>
+				<p>Quando o homem olhou para o céu infinito <br>
+				Percebeu o vazio dentro da existência <br>
+				O poder quantizável de sua consciência <br>
+				A verdade imutável que aqui explicito </p>
+
+				<p>Pois se nosso existir nos precede a essência <br>
+				Se o homem ainda está em conflito <br>
+				Se tua alma ainda lhe volve ao detrito <br>
+				Onde está escondido tua força e potência?</p>
+
+				<p>Talvez nesse teu vício que traz onisciência <br>
+				Continuando a dizer o que já fora dito <br>
+				Que transcendendo a síntese da transcendência</p>
+
+				<p>Até Sísifo deixa de ser tão aflito <br>
+				No absurdo terror dessa vil inocência <br>
+				Pois o homem olhou para seu infinito</p>
+		</section>
+		<section class="blue4">
+		<h2>A noite</h2>
+			<p>Aquela tarde <br>
+			ainda arde <br>
+			em minha alma</p>
+
+			<p style=" margin-left: 100px">E tua boca <br>
+			Me sufoca <br>
+			Nesse trauma</p>
+
+			<p style=" margin-left: 200px">Teus olhos chovem<br>
+			Gotas escorrem<br>
+			Ele me afaga</p>
+
+			<p style=" margin-left: 100px">Neste sufoco<br>
+			Faltando pouco<br>
+			A luz apaga</p>
+
+			<p>E no escuro<br>
+			Vejo o futuro<br>
+			Em plena calma</p>
+
+			<p style=" margin-left: 200px">Sem desespero<br>
+			Ainda espero<br>
+			Que o mar me traga</p>
+
+			<p style=" margin-left: 100px">Todo o sentido<br>
+			Que foi perdido<br>
+			Na tua morte</p>
+
+			<p>Todo o sangue<br>
+			Derramado<br>
+			Naqueles cortes</p>
+
+			<p style="margin-left: 100px">Toda a dor<br>
+			Que emergiu<br>
+			Daquele trauma</p>
+
+			<p  style="margin-left: 30px">Aquela tarde ainda arde em minha alma</p>
+		</section>
+		<section class="blue5">
+		<h2>A tarde</h2>
+			<div class="tarde">
+				<p>Quando a noite chegar <br>
+				<span style="margin-left: 165px">e a lua me olhar</span><br>
+				teus medos virão <br>
+				<span style="margin-left: 130px">Quando eu não estiver</span><br>
+				e tiver pouca fé <br>
+				<span style="margin-left: 130px">em teu coração</span><br>
+				as lágrimas me<span style="margin-left: 125px">afogaram</span><br>
+				<span style="margin-left: 120px">tudo será uma</span><br>
+				<span style="margin-left: 230px">prisão</span></p>
+
+				<p>O verso <span style="margin-left: 30px">esquiva</span> <span style="margin-left: 60px">lascivas</span><br>
+				<span style="margin-left: 60px"> que </span> <span style="margin-left: 60px"> nas mãos </span><br>
+				A chuva <span style="margin-left: 35px"> dança </span> <span style="margin-left: 67px">de Shiva</span></p>
+
+				<p><span style="margin-left: 180px">A morte está viva</span><br>
+				em tudo que morre <br>
+				<span style="margin-left: 180px">A vida está morta</span><br>
+				em tudo que vive <br>
+				<span style="margin-left: 125px">Entretanto insisto</span><br>
+				<span style="margin-left: 255px">em dizer</span><br>
+				ainda vale a pena <br>
+				<span style="margin-left: 180px">viver.</span></p>
+			</div>
+		</section>
+		<section class="blue6">
+			<h2>O fim</h2>
+			<p>Só depois que a luz se ofuscar no vácuo <br>
+			Depois que as estrelas deixarem de brilhar sob o céu <br>
+			Depois que o universo se desfizer em buracos negros <br>
+			Depois que a entropia tender ao infinito e em seguida a zero <br>
+			Só depois disso tudo, meu bem <br>
+			Nosso amor morrerá <br>
+			Junto com a última estrela <br>
+			Numa supernova feita de nossas almas <br>
+			Entrelaçadas no derradeiro beijo quântico <br>
+			Que fechará as portas desse universo gélido <br>
+			Com o calor de nossos lábios</p>
+		</section>
+	</body>
+</html>
